487f97a20c67bdd3f45bc83b513d3f4999387970
Fabrício dos Santos Moreira <dev.fabriciodossantosmoreira@gmail.com>
2025-01-25 23:26:32 -0300

    update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,62 +4,13 @@
  * @version 1.0.0
 */
 
+async function fetchRepositoryData() {
+    const response = await fetch('./data.json')
+        .then((response) => response.json());
 
-const OWNER = 'FabricioDosSantosMoreira';
-const ACCEPTED_REPOSITORIES = [
-    'react-web-calculator', 
-    'DIO.me', 
-    'TicTacToe', 
-    'SENAI-projeto-django', 
-    'XPe-Python-Bootcamp',
-    'PyQT5-Basic-Examples',
-    'PlanetsVsGalaxiesClassifier',
-    'LeetCode-Solutions',
-];
+    console.log(response);
 
-async function fetchRepositoryInfo(url) {
-    try {
-        const response = await fetch(url);
-
-        if (!response.ok) {
-            console.log(
-                `[fetchRepositoryInfo] - - - > Unknown Error! 
-                \tStatus: ${response.status} 
-                \tResponse: ${response}`
-            );
-            return null;
-        }
-        return await response.json();
-
-    } catch (error) {
-        console.log(`
-            [fetchRepositoryInfo] - - - > Unknown Error!
-            \tMessage: ${error.message}`
-        );
-        return null;
-    }
-}
-
-
-async function fetchAndValidateRepositorySocialPreview(url) {
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            console.log(
-                `[fetchAndValidateRepositorySocialPreview] - - - > Unknown Error! 
-                \tStatus: ${response.status} 
-                \tResponse: ${response}`
-            );
-            return false;
-        }
-        return true;
-    } catch (error) {
-        console.log(
-            `[fetchAndValidateRepositorySocialPreview] - - - > Unknown Error! 
-            \tMessage: ${error.message}`
-        );
-        return false;
-    }
+    return response;
 }
 
 
@@ -67,7 +18,6 @@ function renderRepositoryInfo(repositoryInfo, socialPreviewURL, tag) {
     const item = document.createElement('div');
     item.classList.add('card');
 
-    console.log(repositoryInfo);
     item.innerHTML = `
         <a class="" href="${repositoryInfo.url}"></a>
         <img class="card-img" src="${socialPreviewURL}" alt="Repository Preview Image"></img>
@@ -79,25 +29,23 @@ function renderRepositoryInfo(repositoryInfo, socialPreviewURL, tag) {
 }
 
 
-async function buildCardInfo(repositoryName, tag) {
-    let repositoryURL = `https://api.github.com/repos/${OWNER}/${repositoryName}`;
-    let socialPreviewURL = `https://raw.githubusercontent.com/${OWNER}/${repositoryName}/main/img/app.png`;
+async function buildCardInfo(tag) {
+    const infos = await fetchRepositoryData();
 
-    var repositoryInfo = await fetchRepositoryInfo(repositoryURL);
+    for (let info of infos) {
+        console.log(info);
 
-    if (repositoryInfo !== null) {
-        let repositoryName = repositoryInfo.name;
-        let repositoryDescription = repositoryInfo.description;
-        let repositoryHomepage = repositoryInfo.homepage;
-
-        let socialPreviewExists = await fetchAndValidateRepositorySocialPreview(socialPreviewURL);
-        if (!socialPreviewExists) {
-            socialPreviewURL = './assets/img/github-logo.png';
+        // Basically means that the repository doesn't have a real social preview image 
+        if (info.openGraphImageUrl.includes("https://opengraph.githubassets.com/")) {
+            info.openGraphImageUrl = "./assets/img/github-logo.png"
         }
 
-        renderRepositoryInfo(repositoryInfo, socialPreviewURL, tag);
+        let socialPreviewURL = info.openGraphImageUrl;
+            
+        renderRepositoryInfo(info, socialPreviewURL, tag);
     }
-}
+
+};
 
 
 function formatString(string) {
@@ -114,13 +62,11 @@ function formatString(string) {
     }
 
     return newString;
-}
+};
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    const repositoryCardList = document.getElementById('repositoryCardList');
+    const tag = document.getElementById('repositoryCardList');
 
-    for (let repository of ACCEPTED_REPOSITORIES) {
-        buildCardInfo(repository, repositoryCardList);
-    }
+    buildCardInfo(tag);
 });
